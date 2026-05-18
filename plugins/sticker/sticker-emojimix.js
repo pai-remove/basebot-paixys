ea15e31b668cb111ce4a936e9a173adc7bccf873
Paiavaliable <paisuperxo@gmail.com>
2026-05-18 16:29:37 +0700

    Upload from Synera Bot

---
+++ plugins/sticker/sticker-emojimix.js
@@ -0,0 +1,26 @@
+import pkg from "akiraa-wb";
+const {Sticker} = pkg;
+
+let handler = async (m, { conn, text, args }) => {
+  if (!args[0]) throw "*• Example :* .emojimix 😠+😂";
+  let [emoji1, emoji2] = text.split`+`;
+  let anu = await Func.fetchJson(
+    `https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`,
+  );
+  for (let res of anu.results) {
+    let stiker = await Sticker.sticker5(
+      false,
+      res.url,
+      global.packname,
+      global.author,
+    );
+    conn.sendFile(m.chat, stiker, "sticker.webp", "", m);
+  }
+};
+
+handler.help = ["emojimix"].map((a) => a + " *[emoji1+emoji2]*");
+handler.tags = ["sticker"];
+handler.command = ["emojimix"];
+handler.limit = true;
+handler.register = true;
+export default handler;
