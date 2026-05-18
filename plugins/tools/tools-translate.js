3cf927c0dfca23f69b9701855ba36aad223f6066
Paiavaliable <paisuperxo@gmail.com>
2026-05-18 16:30:44 +0700

    Upload from Synera Bot

---
+++ plugins/tools/tools-translate.js
@@ -0,0 +1,45 @@
+import {translate} from "@vitalets/google-translate-api";
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  let q = m.quoted ? m.quoted : m;
+  let isCode = await (await listLanguage()).map((a) => a.code);
+  if (!text)
+    throw `*• Example :* ${usedPrefix + command} *id Good Night*
+
+*[ LIST LANGUAGE ]*
+${await (await listLanguage()).map((a, i) => `> *${i + 1}* ${a.name.toUpperCase()} *[ ${a.code} ]*`).join("\n")}`;
+  let keyword = text.split(" ")[0];
+  let data = q.text.slice(keyword.length + 1);
+  if (isCode.includes(keyword)) m.reply(wait);
+  try {
+    if (!isCode.includes(keyword))
+      throw `*[ LIST LANGUAGE ]*
+${await (await listLanguage()).map((a, i) => `> *${i + 1}* ${a.name.toUpperCase()} *[ ${a.code} ]*`).join("\n")}`;
+    let txt = await (
+      await translate(data, { to: keyword, autoCorrect: true }).catch(
+        (_) => null,
+      )
+    ).text.toString();
+    m.reply(txt);
+  } catch (e) {
+    throw e;
+  }
+};
+handler.help = ["translate", "tr"].map((a) => a + " *[input text]*");
+handler.tags = ["tools"];
+handler.command = ["translate", "tr"];
+
+export default handler;
+
+async function listLanguage() {
+  let data = await (
+    await Func.fetchJson(
+      "https://translate.google.com/translate_a/l?client=webapp&sl=auto&tl=en&v=1.0&hl=en&pv=1&tk=&source=bh&ssel=0&tsel=0&kc=1&tk=626515.626515&q=",
+    )
+  ).sl;
+  const result = Object.entries(data).map(([code, name]) => ({
+    code,
+    name,
+  }));
+  return result;
+}
